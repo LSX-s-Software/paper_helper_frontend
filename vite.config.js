0e2780878bfb3c18015606df19f2645a7b82f0cc
Saltro <2373117747@qq.com>
2022-05-26 12:18:32 +0800

    feat: 添加unplugin-icons

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
@@ -11,12 +13,23 @@ export default defineConfig({
     vue(),
     AutoImport({
       imports: ["vue", "vue-router"],
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(__dirname, "./src/auto-imports.d.ts"),
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ["ep"],
+        }),
+      ],
+      dts: path.resolve(__dirname, "./src/components.d.ts"),
     }),
     eslintPlugin({
       include: ["src/**/*.{js,vue}", "src/*.{js,vue}"],
+    }),
+    Icons({
+      autoInstall: true,
     }),
   ],
   resolve: {
