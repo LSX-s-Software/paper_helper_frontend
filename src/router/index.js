82f7c02f89b993653fc978d2039522f78f892fc6
LSX-s-Software <2682191498@qq.com>
2022-06-21 08:34:33 +0800

    feat: 路由守护

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginRegView from "../views/LoginRegView.vue";
 
+import { ElMessage } from "element-plus";
+
 const routes = [
   {
     path: "/",
@@ -10,11 +12,17 @@ const routes = [
   {
     path: "/home/:projectId",
     name: "home",
+    meta: {
+      needAuth: true,
+    },
     component: () => import("../views/HomeView.vue"),
   },
   {
     path: "/paper/:paperId",
     name: "paper",
+    meta: {
+      needAuth: true,
+    },
     component: () => import("../views/PaperView.vue"),
   },
 ];
@@ -22,6 +30,25 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(to => {
+  console.log(to);
+  if (to.meta.needAuth) {
+    //判断当前路由是否需要进行权限控制
+    let token = localStorage.getItem("token");
+    if (token == null || token == "") {
+      router.replace("/");
+      ElMessage({
+        message: "您未登录",
+        type: "error",
+      });
+      return false;
+    }
+    return true;
+  } else {
+    return true;
+  }
 });
 
 export default router;
